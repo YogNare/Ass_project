b70925ad41798853647d2e4d154402699cebffe8
Andrew-Vetrov <145760988+Andrew-Vetrov@users.noreply.github.com>
2024-04-22 10:29:37 +0700

    Update analytics.c
    добавлены комментарии

--- poker/analytics.c
+++ poker/analytics.c
@@ -13,8 +13,8 @@ extern volatile int COMB1, COMB2, WHO_WIN;
 void analysis() {
     COMB1 = 0, COMB2 = 0;
 
-    int res1[13], res2[13], flags[2] = { 0, 0 };
-    int flash[2] = { 0, 0 };
+    int res1[13], res2[13], flags[2] = { 0, 0 }; // res - массивы для хранения количества карт каждого достоинства
+    int flash[2] = { 0, 0 }; // для обнаружения флеша в комбинации
 
     for (int i = 0; i < 13; i++) {
         res1[i] = 0;
@@ -128,61 +128,61 @@ void analysis() {
     }
 
     if (hand1.cards[0].suit == hand1.cards[1].suit && hand1.cards[0].suit == hand1.cards[2].suit && hand1.cards[0].suit == hand1.cards[3].suit && hand1.cards[0].suit == hand1.cards[4].suit) {
-        flash[0] = 1;
+        flash[0] = 1; // если 5 карт одной масти, то это флеш
     }
 
     if (hand2.cards[0].suit == hand2.cards[1].suit && hand2.cards[0].suit == hand2.cards[2].suit && hand2.cards[0].suit == hand2.cards[3].suit && hand2.cards[0].suit == hand2.cards[4].suit) {
-        flash[1] = 1;
+        flash[1] = 1; // аналогично для второй руки
     }
     
     for (int i = 0; i < 13; i++) {
         if (res1[i] == 1) {
-            if (res1[i + 1] == 1 && res1[i + 2] == 1 && res1[i + 3] == 1 && res1[i + 4] == 1) {
+            if (res1[i + 1] == 1 && res1[i + 2] == 1 && res1[i + 3] == 1 && res1[i + 4] == 1) { // если стрит
                 if (flash[0] == 1) {
-                    if (i == 8) {
-                        COMB1 = 9;
+                    if (i == 8) { // стрит и флеш и первая карта в порядке по возрастанию это 10 => флеш рояль
+                        COMB1 = 9; 
                         break;
                     }
 
-                    else {
+                    else { // если первая карта не 10, то стрит флеш
                         COMB1 = 8;
                         break;
                     }
                 }
 
-                else {
+                else { // стрит, но не флеш
                     COMB1 = 4;
                     break;
                 }
             }
 
             else {
-                if (flash[0] == 1) {
+                if (flash[0] == 1) { // просто флеш
                     COMB1 = 5;
                 }
             }
         }
 
-        if (res1[i] == 4) {
+        if (res1[i] == 4) { // каре
             COMB1 = 7;
         }
 
         if (res1[i] == 2) {
             for (int k = i + 1; k < 13; k++) {
-                if (res1[k] == 2) {
+                if (res1[k] == 2) { // две пары
                     COMB1 = 2;
                     flags[0] = 1;
                     break;
                 }
 
-                if (res1[k] == 3) {
+                if (res1[k] == 3) { // фулл хаус
                     COMB1 = 6;
                     flags[0] = 1;
                     break;
                 }
             }
 
-            if (flags[0] == 0) {
+            if (flags[0] == 0) { // если две пары и фулл хаус не подошли, то это пара
                 COMB1 = 1;
             }
 
@@ -191,14 +191,14 @@ void analysis() {
         
         if (res1[i] == 3) {
             for (int k = i + 1; k < 13; k++) {
-                if (res1[k] == 2) {
+                if (res1[k] == 2) { // фулл хаус
                     COMB1 = 6;
                     flags[0] = 1;
                     break;
                 }
             }
 
-            if (flags[0] == 0) {
+            if (flags[0] == 0) { // три карты одного достоинства, но не фулл хаус => сет
                 COMB1 = 3;
             }
 
@@ -206,7 +206,7 @@ void analysis() {
         }
     }
 
-    for (int i = 0; i < 13; i++) {
+    for (int i = 0; i < 13; i++) { // аналогично для второй руки
         if (res2[i] == 1) {
             if (res2[i + 1] == 1 && res2[i + 2] == 1 && res2[i + 3] == 1 && res2[i + 4] == 1) {
                 if (flash[1] == 1) {
@@ -287,17 +287,17 @@ void analysis() {
 
     if (COMB1 == COMB2) {
         for (int i = 12; i >= 0; i--) {
-            if (res1[i] == 1 && res2[i] == 0) {
+            if (res1[i] == 1 && res2[i] == 0) { // старшая карта у первой руки
                 WHO_WIN = 1;
-                return;
+                return; // идём от старших карт к младшим - если нашли различия у рук в наличии какой-либо карты, то дальше нет смысла искать старшую карту
             }
 
-            if (res2[i] == 1 && res1[i] == 0) {
+            if (res2[i] == 1 && res1[i] == 0) { // старшая карта у второй руки
                 WHO_WIN = 2;
                 return;
             }
         }
 
-        WHO_WIN = 0;
+        WHO_WIN = 0; // достоинства всех карт первой руки совпадают с достоинствами всех карт второй руки => ничья
     }
 }
